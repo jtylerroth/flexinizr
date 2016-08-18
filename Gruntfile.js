0c2b5f9e2e2e76b2e93362ca3c317dfe1f804215
Tyler Roth <jtylerroth@gmail.com>
2016-08-18 16:07:42 -0400

    Removed javascript from project

--- Gruntfile.js
+++ Gruntfile.js
@@ -50,20 +50,12 @@ module.exports = function (grunt) {
                 src: 'src/temp/flexinizr.css',
                 dest: 'src/compiled/flexinizr.css'
             }
-        },
-        uglify: {
-            my_target: {
-                files: {
-                    'dist/flexinizr.min.js': ['src/*.js']
-                }
-            }
         }
     });
 
 
     // Load
     grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-sass');
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-copy');
@@ -71,6 +63,6 @@ module.exports = function (grunt) {
 
 
     // Register
-    grunt.registerTask('default', ['sass', 'postcss', 'copy', 'cssmin', 'uglify']);
+    grunt.registerTask('default', ['sass', 'postcss', 'copy', 'cssmin']);
 
 };
